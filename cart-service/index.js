c66ee34e86c407493972ff76284408ec101bc590
Gayan-98 <dongayanpathirage@gmail.com>
2026-08-04 16:45:59 +0530

    added new

--- cart-service/index.js
+++ cart-service/index.js
@@ -5,7 +5,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// In-memory stored: { [userId]: { [productId]: { productId, quantity } } }
+// In-memorys stored: { [userId]: { [productId]: { productId, quantity } } }
 const carts = {};
 
 function getCart(userId) {
