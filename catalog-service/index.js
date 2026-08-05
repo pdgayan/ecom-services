a0896d019b432311f8c1059d44540e4a6424f351
Gayan-98 <dongayanpathirage@gmail.com>
2026-08-05 15:12:37 +0530

    numeric value bug fixed in catalog

--- catalog-service/index.js
+++ catalog-service/index.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const cors = require("cors");
 const crypto = require("crypto");
+const pg = require("pg");
+pg.types.setTypeParser(1700, (val) => (val === null ? null : parseFloat(val)));
+
 const {
   SecretsManagerClient,
   GetSecretValueCommand,
